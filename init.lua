f5920c2f85bc68d6131adc4961041da0d64619e7
libresoul <dragoofficial991@gmail.com>
2024-04-21 23:19:24 +0530

    options moved to the init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,9 @@
 require("libresoul")
+vim.opt.number = true
+vim.opt.relativenumber = true
+--Autosave upon startup
+vim.g.auto_save = 1
+-- Set indentation to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
--- lua/libresoul/init.lua
+++ lua/libresoul/init.lua
@@ -1,10 +1,2 @@
 require("libresoul.remap")
 require("libresoul.packer")
-vim.opt.number = true
-vim.opt.relativenumber = true
---Autosave upon startup
-vim.g.auto_save = 1
--- Set indentation to 4 spaces
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
