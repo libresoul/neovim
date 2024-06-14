22ac545fe6ee58de093534fc6bc1108b7ed48614
libresoul <dragoofficial991@gmail.com>
2024-06-14 10:37:47 +0530

    snippets added

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,5 +1,7 @@
 local cmp = require("cmp")
 
+require("luasnip.loaders.from_vscode").lazy_load()
+
 cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		['<C-o>'] = cmp.mapping.complete(),
--- lua/libresoul/packer.lua
+++ lua/libresoul/packer.lua
@@ -36,6 +36,8 @@ return require('packer').startup(function(use)
 	use 'hrsh7th/nvim-cmp'
 	use 'hrsh7th/cmp-nvim-lsp'
 	use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
+    use 'rafamadriz/friendly-snippets'
 	use '907th/vim-auto-save'
 	use 'lambdalisue/suda.vim'
 	use "steelsojka/pears.nvim"
