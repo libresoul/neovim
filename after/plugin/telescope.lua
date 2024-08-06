50f1ea7c21581578a8124c1799abe89e9ff8a934
libresoul <dragoofficial991@gmail.com>
2024-08-06 20:00:59 +0530

    make hidden files show up in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,7 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', function ()
+    builtin.find_files({hidden = true})
+end)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pg', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
