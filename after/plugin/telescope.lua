86482df074b0b4e58953a6f2181e03175ef4fdea
libresoul <dragoofficial991@gmail.com>
2024-08-06 21:56:03 +0530

    change telescope bindings
    map <leader>pf to find files and <leader>ph to find files along with
    hidden ones.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', function ()
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ph', function ()
     builtin.find_files({hidden = true})
 end)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
