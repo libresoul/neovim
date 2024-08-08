e914e64812ce5c1768d211a06fedccdd3bf67eba
libresoul <dragoofficial991@gmail.com>
2024-08-08 11:21:35 +0530

    fix netrw line numbers properly

--- init.lua
+++ init.lua
@@ -10,3 +10,4 @@ vim.opt.expandtab = true
 vim.opt.showmode = false
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
 vim.g.copilot_enabled = 0
+vim.g.netrw_bufsettings = 'noma nomod nu nobl nowrap ro'
