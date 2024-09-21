393135e752d2d981d11d3189293d12e875aa7edb
libresoul <dragoofficial991@gmail.com>
2024-09-21 17:46:27 +0530

    add smart indent

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ vim.g.auto_save = 1
 -- Set indentation to 4 spaces
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.showmode = false
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
